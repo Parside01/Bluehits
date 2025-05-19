a7b348cd30db01471675a85edadd0cb626062027
mis_hka_ <mictekaras29@gmail.com>
2025-05-19 12:35:16 +0700

    Сделал адаптивные кнопки(расчитывается от размера экрана) также переделал боковую панель

--- app/src/main/java/com/example/bluehits/ui/Buttons.kt
+++ app/src/main/java/com/example/bluehits/ui/Buttons.kt
@@ -1,118 +1,27 @@
 package com.example.bluehits.ui
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
+
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
 
 
 @Composable
-fun DebugButton(
+fun StyledButton(
+    text: String,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
-
+    modifier: Modifier = Modifier,
+    style: ButtonStyles.ButtonStyle = ButtonStyles.baseButtonStyle()
 ) {
     OutlinedButton(
         onClick = onClick,
-        modifier = modifier
-            .size(width = 75.dp, height = 35.dp),
-        shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.outlinedButtonColors(
-            containerColor = Color.White // фон
-        ),
-        contentPadding = PaddingValues(horizontal = 17.dp, vertical = 1.dp),
-        border = BorderStroke(1.dp, Color.Black)
+        modifier = modifier.then(style.modifier),
+        shape = style.shape,
+        colors = style.colors,
+        contentPadding = style.contentPadding,
+        border = style.border
     ) {
-        Text(
-            text = "Debug",
-            color = Color.Black,
-            fontSize = 12.sp,
-            textAlign = TextAlign.Center
-        )
-    }
-}
-@Composable
-fun RunButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-
-) {
-    OutlinedButton(
-        onClick = onClick,
-        modifier = modifier
-            .size(width = 62.dp, height = 35.dp),
-        shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.outlinedButtonColors(
-            containerColor = Color.White
-        ),
-        contentPadding = PaddingValues(horizontal = 17.dp, vertical = 1.dp),
-        border = BorderStroke(1.dp, Color.Black)
-    ) {
-        Text(
-            text = "Run",
-            color = Color.Black,
-            fontSize = 12.sp,
-            textAlign = TextAlign.Center
-        )
-    }
-}
-@Composable
-fun TrashButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-
-) {
-    OutlinedButton(
-        onClick = onClick,
-        modifier = modifier
-            .size(width = 69.dp, height = 35.dp),
-        shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.outlinedButtonColors(
-            containerColor = Color.White
-        ),
-        contentPadding = PaddingValues(horizontal = 15.dp, vertical = 1.dp),
-        border = BorderStroke(1.dp, Color.Black)
-    ) {
-        Text(
-            text = "Trash",
-            color = Color.Black,
-            fontSize = 12.sp,
-            textAlign = TextAlign.Center
-        )
-    }
-}
-@Composable
-fun Add(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-
-) {
-    OutlinedButton(
-        onClick = onClick,
-        modifier = modifier
-            .size(width = 75.dp, height = 35.dp),
-        shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.outlinedButtonColors(
-            containerColor = Color.White // фон
-        ),
-        contentPadding = PaddingValues(horizontal = 17.dp, vertical = 1.dp),
-        border = BorderStroke(1.dp, Color.Black)
-    ) {
-        Text(
-            text = "add",
-            color = Color.Black,
-            fontSize = 12.sp,
-            textAlign = TextAlign.Center
-        )
+        Text(text = text, style = style.textStyle)
     }
 }
--- app/src/main/java/com/example/bluehits/ui/MainScreen.kt
+++ app/src/main/java/com/example/bluehits/ui/MainScreen.kt
@@ -5,98 +5,148 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.systemBarsPadding
+
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
-import androidx.compose.runtime.*
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import kotlin.math.min
+
+
 @Composable
 fun MainScreen() {
     val textMeasurer = rememberTextMeasurer()
     val blocksManager = remember { BlocksManager() }
     var isPanelVisible by remember { mutableStateOf(false) }
+    val config = LocalConfiguration.current
+    val baseDimension = min(config.screenWidthDp, config.screenHeightDp).dp
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .systemBarsPadding()
+    ) {
+        val (canvas, panel, addButton, debugButton, runButton, trashButton) = createRefs()
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Add(
-            onClick = { isPanelVisible = !isPanelVisible },
+
+        Column(
             modifier = Modifier
-                .offset(x = 500.dp, y = 24.5.dp)
-                .zIndex(1f)
-        )
-
-        DebugButton(
-            onClick = { },
-            modifier = Modifier
-                .offset(x = 707.dp, y = 24.5.dp)
-                .zIndex(1f)
-        )
-        RunButton(
-            onClick = { },
-            modifier = Modifier
-                .offset(x = 635.dp, y = 24.5.dp)
-                .zIndex(1f)
-        )
-        TrashButton(
-            onClick = { },
-            modifier = Modifier
-                .offset(x = 713.dp, y = 284.5.dp)
-                .zIndex(1f)
-        )
-
-        Row(modifier = Modifier.fillMaxSize()) {
-            AnimatedVisibility(
-                visible = isPanelVisible,
-                enter = slideInHorizontally() + fadeIn(),
-                exit = slideOutHorizontally() + fadeOut()
-            ) {
-                ControlPanel(
-                    blocksManager = blocksManager,
-                    modifier = Modifier
-                )
-            }
-
-            Spacer(modifier = Modifier.width(16.dp))
-
-            Column(modifier = Modifier.weight(1f)) {
-                createCanvas(
-                    blocks = blocksManager.uiBlocks,
-                    textMeasurer = textMeasurer,
-                    onDrag = { },
-                    onBlockDrag = { block, delta ->
-                        blocksManager.moveBlock(block, delta)
-                    }
-                )
-            }
+                .constrainAs(canvas) {
+                    start.linkTo(if (isPanelVisible) panel.end else parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+                .zIndex(0f)
+        ) {
+            createCanvas(
+                blocks = blocksManager.uiBlocks,
+                textMeasurer = textMeasurer,
+                onDrag = { },
+                onBlockDrag = { block, delta ->
+                    blocksManager.moveBlock(block, delta)
+                }
+            )
         }
+
+        AnimatedVisibility(
+            visible = isPanelVisible,
+            enter = slideInHorizontally() + fadeIn(),
+            exit = slideOutHorizontally() + fadeOut(),
+            modifier = Modifier.constrainAs(panel) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                width = Dimension.value(180.dp)
+                height = Dimension.fillToConstraints
+            }
+        ) {
+            ControlPanel(
+                blocksManager = blocksManager,
+                modifier = Modifier
+                    .background(
+                        color = Color(0xFFF9F9FF),
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
+            )
+        }
+
+
+        StyledButton(
+            text = "Add",
+            onClick = { isPanelVisible = !isPanelVisible },
+            modifier = Modifier.constrainAs(addButton) {
+                end.linkTo(parent.end, margin = baseDimension * 0.05f)
+                bottom.linkTo(parent.bottom, margin = baseDimension * 0.05f)
+                width = Dimension.wrapContent
+                height = Dimension.wrapContent
+            }
+        )
+
+        StyledButton(
+            text = "Debug",
+            onClick = {},
+            modifier = Modifier.constrainAs(debugButton) {
+                end.linkTo(parent.end, margin = baseDimension * 0.05f)
+                top.linkTo(parent.top, margin = baseDimension * 0.05f)
+                width = Dimension.wrapContent
+                height = Dimension.wrapContent
+            }
+        )
+
+        StyledButton(
+            text = "Run",
+            onClick = {},
+            modifier = Modifier.constrainAs(runButton) {
+                end.linkTo(debugButton.start, margin = baseDimension * 0.02f)
+                top.linkTo(parent.top, margin = baseDimension * 0.05f)
+                width = Dimension.wrapContent
+                height = Dimension.wrapContent
+            }
+        )
+
+
+
+        StyledButton(
+            text = "Trash",
+            onClick = {},
+            style = ButtonStyles.baseButtonStyle(),
+            modifier = Modifier.constrainAs(trashButton) {
+                end.linkTo(parent.end, margin = baseDimension * 0.05f)
+                top.linkTo(debugButton.bottom, margin = baseDimension * 0.03f)
+                width = Dimension.wrapContent
+                height = Dimension.wrapContent
+            }
+
+        )
     }
 }
-
 
 @Composable
 fun ControlPanel(
@@ -104,18 +154,9 @@ fun ControlPanel(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
-            .width(180.dp)
-            .fillMaxHeight()
-            .padding(16.dp)
-            .background(
-                color = Color(0xFFF9F9FF),
-                shape = RoundedCornerShape(16.dp)
-            )
-            .verticalScroll(rememberScrollState()),
+        modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
-    )
-    {
+    ) {
         val buttons = listOf(
             "Int" to "Int",
             "Add" to "Add",
@@ -126,31 +167,11 @@ fun ControlPanel(
         )
 
         buttons.forEach { (blockType, label) ->
-            OutlinedButton(
+            StyledButton(
+                text = label,
                 onClick = { blocksManager.addNewBlock(blockType) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFF3F3FF), shape = RoundedCornerShape(12.dp)),
-                shape = RoundedCornerShape(12.dp),
-                border = BorderStroke(1.dp, Color(0xFF888888)),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    containerColor = Color(0xFFF3F3FF),
-                    contentColor = Color.Black
-                )
-            ) {
-                Text(
-                    text = label,
-                    modifier = Modifier.padding(vertical = 8.dp),
-                    color = Color.Black
-                )
-            }
+                style = ButtonStyles.controlPanelButtonStyle()
+            )
         }
     }
 }
-
-
-
-
-
-
-
