9cc98eae201e6b33e15809790e5bc8f2107b8304
parside <vadimakovlev79449@gmail.com>
2025-05-19 10:15:50 +0700

    Нельзя коннектить пины одинаковых блоков

--- interpreter/src/main/java/com/example/interpreter/blocks/Var.kt
+++ interpreter/src/main/java/com/example/interpreter/blocks/Var.kt
@@ -4,45 +4,75 @@ import com.example.interpreter.models.Block
 import com.example.interpreter.models.ExecutionState
 import com.example.interpreter.models.Id
 import com.example.interpreter.models.PinManager
+import com.example.interpreter.models.VarObserver
+import com.example.interpreter.models.VarState
+import com.example.interpreter.pins.PinBool
+import com.example.interpreter.pins.PinInt
 
 class IntBlock internal constructor(
     id: Id,
-    default: Int = 0
+    default: Int = 0,
+    name: String = "Int"
 ) : Block(
     id,
-    "Int",
+    name,
     mutableListOf(PinManager.createPinInt("a", default, id)),
     mutableListOf(PinManager.createPinInt("b", default, id))
-) {
-    override fun execute(): ExecutionState {
-        if (inputs.size != outputs.size) {
-            throw Exception("Number of outputs must be equal")
-        }
+), VarObserver<Int> {
+    lateinit var varState: VarState<Int>
+    internal fun setVarState(varState: VarState<Int>) {
+        this.varState = varState
+    }
 
-        for (i in inputs.indices) {
-            outputs[i].setValue(inputs[i].getValue())
+    internal val setPin: PinInt = inputs[0] as PinInt
+    internal val getPin: PinInt = outputs[0] as PinInt
+
+    override fun execute(): ExecutionState {
+        if (!::varState.isInitialized) {
+            throw IllegalStateException("IntVarBlock '${name}' (ID: ${id.string()}) not initialized.")
         }
+        val inputValue = setPin.getValue() as Int
+        varState.setValue(inputValue)
         return ExecutionState.COMPLETED
+    }
+
+    override fun onValueChanged(newValue: Int) {
+        getPin.setValue(newValue)
+        setPin.setValue(newValue)
     }
 }
 
 class BoolBlock internal constructor(
     id: Id,
-    default: Boolean = false
+    default: Boolean = false,
+    name: String = "Bool"
 ) : Block(
     id,
-    "Bool",
-    mutableListOf(PinManager.createPinBool("a", default)),
-    mutableListOf(PinManager.createPinBool("b", default))
-) {
+    name,
+    mutableListOf(PinManager.createPinBool("set", default)),
+    mutableListOf(PinManager.createPinBool("get", default))
+), VarObserver<Boolean> {
+    lateinit var varState: VarState<Boolean>
+
+    internal fun setVarState(varState: VarState<Boolean>) {
+        this.varState = varState
+    }
+
+    val setPin: PinBool get() = inputs[0] as PinBool
+    val getPin: PinBool get() = outputs[0] as PinBool
+
     override fun execute(): ExecutionState {
-        if (inputs.size != outputs.size) {
-            throw Exception("Number of outputs must be equal")
+        if (!::varState.isInitialized) {
+            throw IllegalStateException("BoolBlock '${name}' (ID: ${id.string()}) not initialized.")
         }
 
-        for (i in inputs.indices) {
-            outputs[i].setValue(inputs[i].getValue())
-        }
+        val inputValue = setPin.getValue() as Boolean
+        varState.setValue(inputValue)
         return ExecutionState.COMPLETED
+    }
+
+    override fun onValueChanged(newValue: Boolean) {
+        getPin.setValue(newValue)
+        setPin.setValue(newValue)
     }
 }
