7079ddbf497e610fe282dfee34ebf3dbc579fe79
parside <vadimakovlev79449@gmail.com>
2025-05-29 19:39:06 +0700

    Модифицировал FunctionManager

--- interpreter/src/main/java/com/example/interpreter/models/FunctionManager.kt
+++ interpreter/src/main/java/com/example/interpreter/models/FunctionManager.kt
@@ -3,12 +3,15 @@ package com.example.interpreter.models
 import com.example.interpreter.blocks.FunctionCallBlock
 import com.example.interpreter.blocks.FunctionDefinitionBlock
 import com.example.interpreter.blocks.FunctionReturnBlock
+import jdk.internal.util.xml.impl.Input
 
 data class FunctionInfo(
     var name: String,
     var definitionBlock: FunctionDefinitionBlock,
     val returnBlocks: MutableSet<FunctionReturnBlock>,
     val callBlocks: MutableSet<FunctionCallBlock>,
+    val inputs: MutableList<Pin>,
+    val outputs: MutableList<Pin>
 )
 
 object FunctionManager {
@@ -23,19 +26,32 @@ object FunctionManager {
             definition.name,
             definition,
             mutableSetOf(),
-            mutableSetOf()
+            mutableSetOf(),
+            mutableListOf(),
+            mutableListOf()
         )
         functions[definition.name] = info
     }
 
     fun addFunctionReturnBlock(returnBlock: FunctionReturnBlock) {
         if (!functions.containsKey(returnBlock.getFunctionName())) throw IllegalArgumentException("Function definition does not exist")
-        functions[returnBlock.getFunctionName()]?.returnBlocks?.add(returnBlock)
+        val funcInfo = functions[returnBlock.getFunctionName()]
+
+        funcInfo?.returnBlocks?.add(returnBlock)
+        funcInfo?.outputs?.forEach { output ->
+            returnBlock.addInputArg(output)
+        }
     }
 
     fun addFunctionCallBlock(call : FunctionCallBlock) {
         if (!functions.containsKey(call.getFunctionName())) throw IllegalArgumentException("Function definition does not exist")
         functions[call.getFunctionName()]?.callBlocks?.add(call)
+        functions[call.getFunctionName()]?.inputs?.forEach { input ->
+            call.addInputArg(input)
+        }
+        functions[call.getFunctionName()]?.outputs?.forEach { output ->
+            call.addOutputArg(output)
+        }
     }
 
     fun renameFunction(funcName: String, newName: String) {
@@ -66,6 +82,7 @@ object FunctionManager {
         info?.let { info ->
             info.definitionBlock.addOutputArg(arg)
             info.callBlocks.forEach { callBlock -> callBlock.addInputArg(arg) }
+            info.inputs.add(arg)
         }
     }
 
@@ -78,6 +95,7 @@ object FunctionManager {
         info?.let { info ->
             info.callBlocks.forEach { callBlock -> callBlock.addOutputArg(arg) }
             info.returnBlocks.forEach { returnBlock -> returnBlock.addInputArg(arg) }
+            info.outputs.add(arg)
         }
     }
 }
