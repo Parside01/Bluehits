6a70ee373713fbd883e88a1c9de75581421a0bba
parside <vadimakovlev79449@gmail.com>
2025-05-31 07:11:24 +0700

    Cast fix

--- interpreter/src/main/java/com/example/interpreter/blocks/CastBlock.kt
+++ interpreter/src/main/java/com/example/interpreter/blocks/CastBlock.kt
@@ -4,6 +4,7 @@ import com.example.interpreter.models.Block
 import com.example.interpreter.models.ExecutionState
 import com.example.interpreter.models.Id
 import com.example.interpreter.models.PinManager
+import kotlin.math.roundToInt
 import kotlin.reflect.KClass
 import kotlin.reflect.cast
 
@@ -17,11 +18,34 @@ class CastBlock<F : Any, T : Any>(
     mutableListOf(PinManager.createPin("from", ownId = id, type = fromType)),
     mutableListOf(PinManager.createPin("to", ownId = id, type = toType)),
 ) {
+
+    private val converters: Map<Pair<KClass<*>, KClass<*>>, (Any) -> Any?> = mapOf(
+        Pair(Float::class, Int::class) to { (it as Float).roundToInt() },
+        Pair(Double::class, Int::class) to { (it as Double).roundToInt() },
+        Pair(Int::class, Float::class) to { (it as Int).toFloat() },
+        Pair(Double::class, Float::class) to { (it as Double).toFloat() },
+        Pair(Float::class, Double::class) to { (it as Float).toDouble() },
+        Pair(Int::class, Double::class) to { (it as Int).toDouble() },
+        Pair(Any::class, String::class) to { it.toString() },
+        Pair(String::class, Int::class) to { (it as String).toIntOrNull() },
+        Pair(String::class, Float::class) to { (it as String).toFloatOrNull() },
+        Pair(String::class, Double::class) to { (it as String).toDoubleOrNull() },
+    )
+
     override fun execute(): ExecutionState {
         val rawValue: Any? = inputs.first().getValue()
+        if (rawValue == null) return ExecutionState.COMPLETED
 
-        val valueAsF: F = fromType.cast(rawValue)
-        val valueAsT: T = toType.cast(valueAsF)
+        val converter = converters[Pair(fromType, toType)]
+
+        val valueAsT: T = if (converter != null) {
+            converter(rawValue) as? T
+                ?: throw IllegalArgumentException("Failed to cast from ${fromType.simpleName} to ${toType.simpleName}")
+        } else if (toType.isInstance(rawValue)) {
+            toType.cast(rawValue)
+        } else {
+            throw IllegalArgumentException("Unsupported cast from ${fromType.simpleName} to ${toType.simpleName}\"")
+        }
 
         outputs[0].setValue(valueAsT)
         return ExecutionState.COMPLETED
